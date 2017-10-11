29c72a1733024a44fb7d4ad917479afd44d0e1f8
Electron <31048230+JacksonDuvan@users.noreply.github.com>
2017-10-11 13:55:32 -0500

    Add files via upload

---
+++ processing.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+Servo mi_servo;
+void setup() {
+  Serial.begin(9600);
+
+  mi_servo.attach(8);
+}
+void loop() {
+  if (Serial.available()){
+
+    char angulo = Serial.read();
+
+    if (angulo >= 0 && angulo <= 180){
+      mi_servo.write(angulo);
+      delay(10);      
+      }
+   }
+}
+
